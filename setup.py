2f72ebf8cc40d5fe30c4df6b6e7985be7e7651d8
sergey@telminov.ru <sergey@telminov.ru>
2016-07-01 14:51:04 +0300

    package structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+# python setup.py sdist register upload
+from setuptools import setup, find_packages
+
+
+def get_requires():
+    with open('requirements.txt') as requirements_file:
+        return requirements_file.readlines()
+
+setup(
+    name='grammar_text',
+    version='0.0.1',
+    description='Text with grammar.',
+    author='Telminov Sergey',
+    author_email='sergey@telminov.ru',
+    url='https://github.com/telminov/grammar-text',
+    include_package_data=True,
+    packages=find_packages(),
+    license='The MIT License',
+    install_requires=get_requires()
+)
