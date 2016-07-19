7f7e70263770057f21c4a35eb560420d1fd301f8
sergey@telminov.ru <sergey@telminov.ru>
2016-07-19 18:05:53 +0300

    огрнаничение фраз

--- grammar_text/forms.py
+++ grammar_text/forms.py
@@ -8,12 +8,20 @@ class PhraseWidget(forms.TextInput):
     class Media:
         js = ('grammar_text/main.js', )
 
+    def __init__(self, *args, **kwargs):
+        phrases_qs = kwargs.pop('phrases_qs', None)     # limitation of phrase choices
+        super(PhraseWidget, self).__init__(*args, **kwargs)
+        self.phrases_qs = phrases_qs
+
     def render(self, name, value, attrs=None):
         c = {
             'input_html': super(PhraseWidget, self).render(name, value, attrs=attrs),
             'phrases_url': reverse('phrases-list'),
             'input_id': attrs['id'],
         }
+        if self.phrases_qs:
+            c['phrases_url'] += '?' + '&'.join(['id=%s' % id for id in self.phrases_qs.values_list('id', flat=True)])
+
         html = render_to_string('grammar_text/widget.html', c)
         return html
 
--- grammar_text/views.py
+++ grammar_text/views.py
@@ -7,3 +7,9 @@ from . import serializers
 class PhraseViewset(viewsets.ReadOnlyModelViewSet):
     queryset = models.Phrase.objects.all()
     serializer_class = serializers.Phrase
+
+    def filter_queryset(self, *args, **kwargs):
+        queryset = super(PhraseViewset, self).filter_queryset(*args, **kwargs)
+        if self.request.GET.get('id'):
+            queryset = queryset.filter(id__in=self.request.GET.getlist('id'))
+        return queryset
